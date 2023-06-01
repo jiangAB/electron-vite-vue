9c526ac1cf81cf55a5a9f13aa0da3e276454ca44
jiangAB <jiangjin514@foxmail.com>
2023-06-01 10:00:19 +0800

    feat:update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
 import Home from './components/Home/Home.vue'
-import  testApi  from './sql/sql-api/test.js'
-testApi.addTest({
-    type:1,
-    tel:'177****1111',
-    cookie:'dsjadljsakldjsakldjkaslfklaj',
-})
-.then(res=>{
-console.log(res);
-})
-
-
 </script>
 
 <template>
